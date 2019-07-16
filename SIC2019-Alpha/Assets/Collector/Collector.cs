f592692c6ee42ceb07c83d0727748ab4bee1ffcd
Lucas Pena <Lucaslpena@gmail.com>
2019-07-16 16:26:29 +0200

    echo printing from MIDAS

--- SIC2019-Alpha/Assets/Collector/Collector.cs
+++ SIC2019-Alpha/Assets/Collector/Collector.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class Collector : MonoBehaviour {
 
     private static Collector _instance;
-    public static Collector Instance { get { return _instance; }
+    public static Collector Instance { get { return _instance; } }
+
+
+   public float repeatFrecuencySeconds;
+
+
+    public MidasListener midasListener;
+
     private void Awake()
     {
         if (_instance != null && _instance != this)
@@ -14,15 +21,20 @@ public class Collector : MonoBehaviour {
         } else {
             _instance = this;
         }
-
-        getData();
     }
 
-    public MidasListener midasListener;
 
 
-    public void getData()
+    public void GetData()
     {
         Debug.Log(midasListener.data[0]);
+    }
+
+    void Update() {
+//        GetData();
+    }
+
+    protected void Start() {
+        InvokeRepeating ("GetData", 0f, repeatFrecuencySeconds);
     }
 }
