2f45dac7ecbb1cb0126d4de4ac23991fa7ecba14
Marco Musto <marco.musto@gmail.com>
2019-07-16 13:08:43 +0200

    started the creation of CameraBehaviour script

---
+++ SIC2019-Alpha/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+
+    private float _speed;
+    private Vector3 _startPosition;
+    private Vector3 _stopPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = _startPosition;
+        _stopPosition = _startPosition + new Vector3(0, 0, 5000f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, _stopPosition, Time.deltaTime);   
+    }
+
+}
