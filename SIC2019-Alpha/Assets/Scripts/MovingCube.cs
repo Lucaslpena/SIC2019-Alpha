5cc1e27533f4dfa314bc35c1bc6a3bb1b841c4d9
hectorcarral <hectorcarral@icloud.com>
2019-07-17 17:04:18 +0200

    Update Unity project for better MIDAS integration

---
+++ SIC2019-Alpha/Assets/Scripts/MovingCube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCube : MonoBehaviour
+{
+    public float minSpeed;
+    public float maxSpeed;
+
+	float speed = 5f;
+    int direction = 1;
+
+    void Start()
+    {
+        
+    }
+		
+    void Update()
+    {
+        if (transform.localPosition.x >= 5f)
+            direction = -1;
+        else if (transform.localPosition.x <= -5f)
+            direction = 1;
+
+        float newPosX = transform.localPosition.x + speed * direction * Time.deltaTime;
+        transform.localPosition = new Vector3(newPosX, transform.localPosition.y, transform.localPosition.z);
+    }
+}
