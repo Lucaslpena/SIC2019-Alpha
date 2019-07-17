46156d2252393d226b0057f915fc4842c02923e8
Lucas Pena <Lucaslpena@gmail.com>
2019-07-17 11:41:43 +0200

    HRV public from struct in singleton

--- SIC2019-Alpha/Assets/Collector/Collector.cs
+++ SIC2019-Alpha/Assets/Collector/Collector.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Collector : MonoBehaviour {
 
+    struct Reading{
+        public double[] eegData;
+        public int serial;
+    }
+
     private static Collector _instance;
     public static Collector Instance { get { return _instance; } }
 
+    public int HRV;
 
-   public float repeatFrecuencySeconds;
 
 
+
+   public double repeatFrecuencySeconds;
     public MidasListener midasListener;
-
     private void Awake()
     {
         if (_instance != null && _instance != this)
@@ -24,16 +30,25 @@ public class Collector : MonoBehaviour {
     }
 
 
-
     public void GetData()
     {
-        Debug.Log(midasListener.data[0]);
-    }
+        Reading r;
+        double[] recentAffective = midasListener.data;
+        r.eegData= recentAffective);
 
-    void Update() {
-//        GetData();
-    }
+        /** Here we will get data from Alayna
+        and add it to the dictionary object... we need to change the dictionary object
 
+            r.serial = // alaynas input //
+
+
+            ..
+             doing daa stuff ere
+
+        **/
+
+        HRV = r.eegData[0]; //this is fake and obviously should be tarnsformed in something mentioned above...
+    }
     protected void Start() {
         InvokeRepeating ("GetData", 0f, repeatFrecuencySeconds);
     }
