832f283e412faf2f3ac4c2c907cd3c93ab20b812
hectorcarral <hectorcarral@icloud.com>
2019-07-17 16:40:38 +0200

    Update MIDAS and ECG stuff

--- MIDAS/ecg_node.py
+++ MIDAS/ecg_node.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
 import sys
-import ecg_utilities
+import numpy as np
+import heartpy
 
 from midas.node import BaseNode
 from midas import utilities as mu
+
+def _mean_hr_heartpy(x, sampling_rate):
+        print("computing mean_hr_heartpy")
+        working_data, measures = heartpy.process(np.array(x), sample_rate=sampling_rate)
+        print(measures['bpm'])
+        return measures['bpm']
 
 
 # ECG processing node
@@ -16,6 +23,8 @@ class ECGNode(BaseNode):
 
         # Append function handles to the metric_functions-list
         self.metric_functions.append(self.mean_hr)
+        self.metric_functions.append(self.mean_hr_heartpy)
+        self.metric_functions.append(self.computeChangeHRV)
 
     def mean_hr(self, x):
         """ Calculate the average heart rate
@@ -23,9 +32,34 @@ class ECGNode(BaseNode):
             obtaining the RR-intervals using
             R-peak detection.
         """
+        print("computing mean_hr")
         rr = ecg_utilities.detect_r_peaks(x['data'][0],
                                           self.primary_sampling_rate)
         return ecg_utilities.hrv_mean_hr(rr)
+    
+    def mean_hr_heartpy(self, x):
+        print("computing mean_hr_heartpy")
+        working_data, measures = heartpy.process(np.array(x['data'][0]), sample_rate=self.primary_sampling_rate)
+        print(measures['bpm'])
+        return measures['bpm']
+    
+    def computeChangeHRV(self, x):
+        """ Returns the change in HRV, computed using averages of HRV of two overlapping time windows. """
+        """ It uses a short window and a long window and compares the difference. """
+        """ The long window has to be equal or shorther than the time window that """
+        """ this function receives (usually equal), and the short window has to be """
+        """ equal or shorter than the long window (usually shorter). """
+        short_time_window = 10 # seconds
+        long_time_window = 30 # seconds
+        
+        data_long = x['data'][0]
+        data_short = data_long[len(data_long) - int(len(data_long)*(short_time_window/long_time_window)):]
+        
+        if len(data_short) > 0 and len(data_long) > 0:
+            print("computing change in HR")
+            return _mean_hr_heartpy(data_short, self.primary_sampling_rate) / _mean_hr_heartpy(data_long, self.primary_sampling_rate)
+        else:
+            return 0
 
 
 # Run the node from command line
--- MIDAS/streamBitalinoECG.py
+++ MIDAS/streamBitalinoECG.py
@@ -9,13 +9,16 @@ from bitalino import BITalino
 
 
 #macAddress = "98:D3:31:90:3D:F0" # Not possible on macOS
-macAddress = "/dev/tty.bitalino-DevB"
+macAddress = "/dev/cu.BITalino-01-99-DevB"
 batteryThreshold = 30
 acqChannels = [0,1,2,3,4,5]
-defaultChannel=4;
+defaultChannel = 4;
 samplingRate = 1000
-nSamples = 10
+nSamples = 1000
 digitalOutput = [1,1,0,0]
+
+analogChannel = 2
+
 # Connect to BITalino
 connection=0
 while connection is 0:
@@ -47,8 +50,12 @@ device.start(samplingRate, acqChannels)
 # Read samples
 while 1:
 	data=device.read(nSamples)
-	print(data[0][defaultChannel+3])
-	outlet.push_sample([data[0][defaultChannel+3]])
+	#print(data[0][defaultChannel + analogChannel])
+	data_channel = []
+	for i in range(len(data)):
+		data_channel.append(data[i][defaultChannel + analogChannel])
+	print(data_channel[0])
+	outlet.push_chunk(data_channel)
 
 # Turn BITalino led on
 device.trigger(digitalOutput)
